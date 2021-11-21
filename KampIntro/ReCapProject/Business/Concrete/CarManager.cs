d4f10531214430363ff80391cf4c3a2d43c555a3
EmirhanAltuntas <91732560+EmirhanAltuntas@users.noreply.github.com>
2021-11-21 23:15:53 +0300

    Add files via upload

---
+++ KampIntro/ReCapProject/Business/Concrete/CarManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
+        public void Add(Car car)
+        {
+            if (car.Description.Length<=2 && car.DailyPrice<=0)
+            {
+                Console.WriteLine("Hata");
+            }
+        }
+    }
+}
